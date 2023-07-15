abc156a7a4d2dd0fba0635aa3574f34b5d0df1cd
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-07-15 08:58:38 +0530

    Starting Again :(

---
+++ src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
@@ -0,0 +1,14 @@
+package io.yadnyesh.teluskoyoutube;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        log.info("Finally Worked");
+        float marks = 6.5f;
+        log.info(String.valueOf(marks));
+
+    }
+}
