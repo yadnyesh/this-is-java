912f3626ea6855bd658c0cfa672cab3babc3eb82
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-08-13 19:31:14 +0530

    while Loop

--- src/main/java/io/yadnyesh/teluskoyoutube/ByteHello.java
+++ src/main/java/io/yadnyesh/teluskoyoutube/ByteHello.java
@@ -2,7 +2,10 @@ package io.yadnyesh.teluskoyoutube;
 
 public class ByteHello {
     public static void main(String[] args) {
-            for (int i = 0; i < 5; i++)
-                System.out.println("Hi!");
+            int i = 1;
+            while (i < 10) {
+                System.out.println("Hi " + i);
+                i++;
+            }
         }
 }
