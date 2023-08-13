506e1309285892125b60c698f699d0335079ed64
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-08-13 18:37:13 +0530

    Starting Again :(

---
+++ src/main/java/io/yadnyesh/teluskoyoutube/ByteHello.java
@@ -0,0 +1,9 @@
+package io.yadnyesh.teluskoyoutube;
+
+public class ByteHello {
+    public static void main(String[] args) {
+        byte b = 127;
+        int a = b;
+        System.out.println(a);
+    }
+}
