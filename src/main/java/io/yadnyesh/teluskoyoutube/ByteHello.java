1351a88ec9bef3c9a4ed1de422ed05dea393379b
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-08-13 19:05:18 +0530

    For Loop

--- src/main/java/io/yadnyesh/teluskoyoutube/ByteHello.java
+++ src/main/java/io/yadnyesh/teluskoyoutube/ByteHello.java
@@ -2,35 +2,7 @@ package io.yadnyesh.teluskoyoutube;
 
 public class ByteHello {
     public static void main(String[] args) {
-        int x = 6;
-        int result = 0;
-
-        switch (x) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Enter a valid number");
-
+            for (int i = 0; i < 5; i++)
+                System.out.println("Hi!");
         }
-
-    }
 }
