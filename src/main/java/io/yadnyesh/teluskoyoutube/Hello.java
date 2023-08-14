94c693e020677444676958f26b474f672e2b0d57
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-08-14 10:41:51 +0530

    Print multi-dimensional array

--- src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
+++ src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
@@ -12,7 +12,20 @@ public class Hello {
 
         for(int i = 0; i < 3; i++) {
             for (int j = 0; j < 4; j++) {
+                nums[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
                 System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int n[]: nums) {
+            for (int m: n) {
+                System.out.print(m + " ");
             }
             System.out.println();
         }
