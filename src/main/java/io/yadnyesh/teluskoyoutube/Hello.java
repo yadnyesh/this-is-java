419269b742384fbfdf3dfdee96640d706f1d4f1e
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-08-14 10:27:16 +0530

    Array

--- src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
+++ src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
@@ -5,9 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        log.info("Finally Worked");
-        float marks = 6.5f;
-        log.info(String.valueOf(marks));
+        int marks[] = {5,6,7};
+        log.info(String.valueOf(marks[1]));
     }
 }
