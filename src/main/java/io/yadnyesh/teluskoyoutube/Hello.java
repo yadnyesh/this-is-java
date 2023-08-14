429af7149d0f9ccc65e1c9c800c015e3133102ef
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-08-14 10:32:57 +0530

    Print multi-dimensional array

--- src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
+++ src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
@@ -7,5 +7,15 @@ public class Hello {
     public static void main(String[] args) {
         int marks[] = {5,6,7};
         log.info(String.valueOf(marks[1]));
+
+        int nums[][] = new int[3][4];
+
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
