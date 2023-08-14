4072b8807f9ffcd6197270d02e8626531ccb9686
yadnyesh <yadnyesh.juvekar@walmart.com>
2023-08-14 11:06:32 +0530

    New For Loop

--- src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
+++ src/main/java/io/yadnyesh/teluskoyoutube/Hello.java
@@ -16,18 +16,8 @@ public class Hello {
             }
         }
 
-        for(int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(nums[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        for (int n[]: nums) {
-            for (int m: n) {
-                System.out.print(m + " ");
-            }
-            System.out.println();
+        for (int n : marks) {
+            System.out.println(n);
         }
 
     }
