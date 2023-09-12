807a701101e00adbd1aeb498a54772502e49de9c
yadnyesh <yadnyesh.juvekar@gmail.com>
2023-09-12 21:24:12 +0530

    Abstract Class

--- src/main/java/io/yadnyesh/teluskoyoutube/Demo.java
+++ src/main/java/io/yadnyesh/teluskoyoutube/Demo.java
@@ -1,18 +1,24 @@
 package io.yadnyesh.teluskoyoutube;
 
-import io.yadnyesh.teluskoyoutube.calc.Calc;
+abstract class Car {
+    public abstract void drive();
+    public void playMusic() {
+        System.out.println("I'm playing music inside the Car");
+    }
+}
+
+class WagonR extends Car {
+    public void drive() {
+        System.out.println("Driving WagonR");
+    }
+}
+
 
 public class Demo {
     public static void main(String[] args) {
-        Calc calc = new Calc();
-        System.out.println("Sum of 10 + 12 is: " + calc.add(10, 12));
-        System.out.println("Difference of 12 and 19 is: " + calc.sub(12, 10));
-
-        ScientificCalc advCalc = new ScientificCalc();
-        System.out.println(advCalc.multi(2,2));
-        System.out.println(advCalc.add(2,5));
-        System.out.println(advCalc.power(3,3));
-
+        Car m800 = new WagonR();
+        m800.playMusic();
+        m800.drive();
     }
 }
 
