ff8577288851fead2bde5ac2bcd92880ad51856e
yadnyesh <yadnyesh.juvekar@gmail.com>
2023-10-07 18:21:34 +0530

    Fibonacci with recursion.

---
+++ src/main/java/io/yadnyesh/dsguyudemy/Fibonacci.java
@@ -0,0 +1,19 @@
+package io.yadnyesh.dsguyudemy;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n;
+        System.out.printf("Fibo is " + fibo(3));
+    }
+
+    public static int fibo(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n - 1;
+        }
+        else
+            return fibo(n - 1) + fibo (n-2);
+    }
+}
--- src/main/java/io/yadnyesh/multiThreadingUdemy/ThreadMain.java
+++ src/main/java/io/yadnyesh/multiThreadingUdemy/ThreadMain.java
@@ -1,7 +1,6 @@
 package io.yadnyesh.multiThreadingUdemy;
 
 import java.math.BigInteger;
-import java.util.function.BiFunction;
 
 public class ThreadMain {
     public static void main(String[] args) {
