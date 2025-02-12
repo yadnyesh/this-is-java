5b8df2f4205aafee30737e2a66dac77460825318
yadnyesh <yadnyesh.juvekar@gmail.com>
2025-02-12 19:58:21 +0530

    Threads in Java

---
+++ src/main/java/io/yadnyesh/conceptandcoding/MultithreadingLearning.java
@@ -0,0 +1,7 @@
+package io.yadnyesh.conceptandcoding;
+
+public class MultithreadingLearning {
+    public static void main(String[] args) {
+        System.out.println("Thread name: " + Thread.currentThread().getName());
+    }
+}
