c6467e9b73d0cba2a08a268b01882b3c665c5408
yadnyesh <yadnyesh.juvekar@gmail.com>
2025-02-27 10:52:37 +0530

    Threads in Java

---
+++ src/main/java/io/yadnyesh/javanotesforprofessionals/Member.java
@@ -0,0 +1,47 @@
+package io.yadnyesh.javanotesforprofessionals;
+
+public class Member {
+    private String name;
+    private String type;
+    private int level;
+    private int rank;
+
+    public Member(String name, String type, int level, int rank) {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
---
+++ src/main/java/io/yadnyesh/javanotesforprofessionals/Team.java
@@ -0,0 +1,18 @@
+package io.yadnyesh.javanotesforprofessionals;
+
+public class Team {
+    Member member;
+
+    public Team(Member member) {
+        this.member = member;
+    }
+
+    public static void main(String[] args) {
+        Member myMember = new Member("Yadnyesh", "forward", 10, 1);
+        Team myTeam = new Team(myMember);
+        System.out.println(myTeam.member.getName());
+        System.out.println(myTeam.member.getType());
+        System.out.println(myTeam.member.getLevel());
+        System.out.println(myTeam.member.getRank());
+    }
+}
