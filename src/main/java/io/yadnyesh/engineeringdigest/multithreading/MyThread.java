c3bd7c4fe4639de6b81938d982dd319e28674772
yadnyesh <yadnyesh.juvekar@gmail.com>
2025-03-21 11:49:13 +0530

    Threads in Java

--- src/main/java/io/yadnyesh/engineeringdigest/multithreading/MyThread.java
+++ src/main/java/io/yadnyesh/engineeringdigest/multithreading/MyThread.java
@@ -4,17 +4,24 @@ public class MyThread extends Thread{
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " is running.");
-            Thread.yield();
+        System.out.println("RUNNING");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
+        System.out.println(t1.getState());
         t1.start();
-        t2.start();
+        System.out.println(t1.getState());
+        Thread.sleep(100);
+        System.out.println(t1.getState());
+        t1.join();
+        System.out.println(t1.getState());
+
     }
 
 
