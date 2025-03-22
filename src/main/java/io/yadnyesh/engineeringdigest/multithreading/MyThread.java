bf7f63dcd7dd2a0e6f6e9217608bcd9671aa8403
yadnyesh <yadnyesh.juvekar@gmail.com>
2025-03-22 10:56:41 +0530

    Threads in Java

--- src/main/java/io/yadnyesh/engineeringdigest/multithreading/MyThread.java
+++ src/main/java/io/yadnyesh/engineeringdigest/multithreading/MyThread.java
@@ -2,23 +2,31 @@ package io.yadnyesh.engineeringdigest.multithreading;
 
 public class MyThread extends Thread{
 
+    public MyThread(String name) {
+        super(name);
+    }
+
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() +  " -> Thread is running ....");
         for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " <--> " + Thread.currentThread().getPriority() + " -> Thread is running ....");
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(i);
+
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        MyThread t1 = new MyThread();
-        t1.start();
-        t1.join();
+        MyThread l = new MyThread("Low Priority Thread");
+        MyThread m = new MyThread("Medium Priority Thread");
+        MyThread h = new MyThread("High Priority Thread");
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.NORM_PRIORITY);
+        h.setPriority(Thread.MAX_PRIORITY);
+
         System.out.println("Hello post t1 thread execution is completed");
     }
 }
