489a752183897d088844df4289849cc1fe1a56b7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2025-06-02 11:16:04 +0530

    todays commit

---
+++ src/main/java/io/yadnyesh/conceptandcoding/MainAnnotation.java
@@ -0,0 +1,7 @@
+package io.yadnyesh.conceptandcoding;
+
+public class MainAnnotation {
+    public static void main(String[] args) {
+        System.out.println(new TestClass().getClass().getAnnotations());
+    }
+}
---
+++ src/main/java/io/yadnyesh/conceptandcoding/TestClass.java
@@ -0,0 +1,5 @@
+package io.yadnyesh.conceptandcoding;
+
+@MyCustomAnnotationWithInherited
+public class TestClass {
+}
