eefe3dfb4a260868dae4211d01b46af6fa3fc23d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2025-06-02 12:17:20 +0530

    Today

--- src/main/java/io/yadnyesh/conceptandcoding/MainAnnotation.java
+++ src/main/java/io/yadnyesh/conceptandcoding/MainAnnotation.java
@@ -1,7 +1,19 @@
 package io.yadnyesh.conceptandcoding;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class MainAnnotation {
-    public static void main(String[] args) {
-        System.out.println(new TestClass().getClass().getAnnotations());
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class eagleClass = Eagle.class;
+        System.out.println(eagleClass.getName());
+        System.out.println(Modifier.toString(eagleClass.getModifiers()));
+        Method[] methods = eagleClass.getMethods();
+        for (Method method: methods) {
+            System.out.println("Method Name: " + method.getName());
+            System.out.println("Return Type: " + method.getReturnType());
+            System.out.println("Class Name: " + method.getDeclaringClass());
+            System.out.println("********");
+        }
     }
 }
