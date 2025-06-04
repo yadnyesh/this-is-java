d96d684d7344f754e4375eca717e862c1957291c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2025-06-04 21:18:25 +0530

    Today

---
+++ src/main/java/io/yadnyesh/jennyslectures/ScannerDemo.java
@@ -0,0 +1,11 @@
+package io.yadnyesh.jennyslectures;
+
+import java.io.IOException;
+
+public class ScannerDemo {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello, World!");
+        int inputx = System.in.read();
+        System.out.println(inputx);
+    }
+}
