be2b1dac5d961ea1c55a7a6f1d04a65c5b19e0c5
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2025-06-06 16:52:42 +0530

    Today

--- src/main/java/io/yadnyesh/jennyslectures/ScannerDemo.java
+++ src/main/java/io/yadnyesh/jennyslectures/ScannerDemo.java
@@ -5,13 +5,9 @@ import java.util.Scanner;
 
 public class ScannerDemo {
     public static void main(String[] args) throws IOException {
-        System.out.println("Welcome to Youtube channel name generator!");
-        System.out.println("What is your nick name?");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("What is the next word you want to add?");
-        String nextWord = scanner.nextLine();
-        System.out.println("Your channel name is: " + name + " " + nextWord);
+        int a = 10;
+        System.out.println(a++ - ++a);
+        System.out.println(++a - a++);
 
     }
 }
