707030474989141abeb2cff9f4ba2f4e4d68e257
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2025-06-06 09:22:26 +0530

    Today

--- src/main/java/io/yadnyesh/jennyslectures/ScannerDemo.java
+++ src/main/java/io/yadnyesh/jennyslectures/ScannerDemo.java
@@ -1,11 +1,15 @@
 package io.yadnyesh.jennyslectures;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ScannerDemo {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello, World!");
-        int inputx = System.in.read();
-        System.out.println(inputx);
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.next();
+        System.out.println("The name you entered is: " + firstName);
+        String secondName = scanner.next();
+        System.out.println("The second name you entered is: " + secondName);
     }
 }
