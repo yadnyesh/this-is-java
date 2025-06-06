d9c25e7e87ad2e563bb784c814208dcf58bfce59
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2025-06-06 17:35:45 +0530

    Today

---
+++ src/main/java/io/yadnyesh/jennyslectures/AssignmentRelationalLogicalDemo.java
@@ -0,0 +1,9 @@
+package io.yadnyesh.jennyslectures;
+
+public class AssignmentRelationalLogicalDemo {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = a;
+        System.out.println("b = " + b);
+    }
+}
