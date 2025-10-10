0a61c0670124c642d0fbfbe778ba5b8849641207
yadnyesh <yadnyesh.juvekar@gmail.com>
2025-10-10 09:24:39 +0530

    First commit from my Macbook.

--- src/main/java/io/yadnyesh/App.java
+++ src/main/java/io/yadnyesh/App.java
@@ -7,6 +7,6 @@ package io.yadnyesh;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!, from my Macbook, finally!" );
     }
 }
